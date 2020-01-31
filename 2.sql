e896276ac40db3fd89180186b9ce540236b6404e
TheAngelFirst <angel-first@bk.ru>
2020-01-31 17:45:46 +0300

    Create 2.sql

---
+++ 2.sql
@@ -0,0 +1,40 @@
+/*
+1.Вычислить 2 переменные: самый дешёвый продукт, производимый бабушкой и дедушкой. Сравнить эти переменные. 
+  Вывести строку "Самый дешёвый продукт делает %MAKER_TYPE%", либо сообщение о равенстве цен самых дешёвых продуктов.
+2.Если средняя цена на блины меньше, чем средняя на пирожки, вывести сообщение: 
+  «В среднем, блины дешевле пирожков на X руб», где X – абсолютное значение разницы между средней ценой пирожка и средней ценой блина. 
+  Если средняя цена на блины больше или равна средней цены на пирожки, вывести сообщение: «блины стоят не меньше пирожков».
+*/
+--1.Вычислить 2 переменные: самый дешёвый продукт, производимый бабушкой и дедушкой. Сравнить эти переменные. Вывести строку "Самый дешёвый продукт делает %MAKER_TYPE%", либо сообщение о равенстве цен самых дешёвых продуктов.
+declare
+	cheapest_gpa varchar2(32);
+	cheapest_gma varchar2(32);
+begin
+	select PRODUCT_NAME into cheapest_gpa from PIES where ROWNUM <= 1 and MAKER_TYPE = 'ДЕДУШКА' order by PRICE asc;
+	select PRODUCT_NAME into cheapest_gma from PIES where ROWNUM <= 1 and MAKER_TYPE = 'БАБУШКА' order by PRICE asc;
+	if cheapest_gpa < cheapest_gma then
+		dbms_output.put_line('Самый дешёвый продукт делает ДЕДУШКА');
+	elsif cheapest_gpa > cheapest_gma
+		dbms_output.put_line('Самый дешёвый продукт делает БАБУШКА');
+	elsif cheapest_gpa = cheapest_gma
+		dbms_output.put_line('Минимальные цены равны');
+	else
+		dbms_output.put_line('1');
+	end if;
+end;
+
+--2.Если средняя цена на блины меньше, чем средняя на пирожки, вывести сообщение: «В среднем, блины дешевле пирожков на X руб», где X – абсолютное значение разницы между средней ценой пирожка и средней ценой блина. Если средняя цена на блины больше или равна средней цены на пирожки, вывести сообщение: «блины стоят не меньше пирожков».
+declare
+	avg_prise_pies number(5);
+	avg_prise_panc number(5);
+begin
+	select avg(PRICE) into avg_prise_pies from PIES;
+	select avg(PRICE) into avg_prise_panc from PANCAKES;
+	if avg_prise_pies > avg_prise_panc then
+		dbms_output.put_line('В среднем, блины дешевле пирожков на ',ABS(avg_prise_pies-avg_prise_panc),' руб');
+	elsif avg_prise_panc >= avg_prise_pies then
+		dbms_output.put_line('блины стоят не меньше пирожков');
+	else
+		dbms_output.put_line('1');
+	end if;
+end;
